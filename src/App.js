2d26182b82cd360a465dcc03b81a0fdffe7dcfee
augustoibarrola <augusto.t.ibarrola@gmail.com>
2020-11-30 11:23:06 -0500

    general commit

--- src/App.js
+++ src/App.js
@@ -12,23 +12,31 @@ const App = () => {
     {value: 3, name: "C" }
 ]
 
+console.log(process.env.REACT_APP_CLIENT_ID)
+
 const [token, setToken] = useState('')
 
 useEffect(() => {
-  fetch('https://accounts.spotify.com/api/token', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-      'Authorization': 'Basic' + btoa('a8e7bf99837d4be583b6775b84b5ec87' + ':' + '713c72bae95a479caddcef458718abac')
-    },
-    data: 'grant_type=client_credentials'
-  })
-  .then(response => {
-    console.log(response)
-    console.log(response.data)
-    console.log(response.data.access_token)
-    setToken(response.data.access_token)
-  })
+  axios('https://accounts.spotify.com/api/token', {
+      headers: {
+        'Content-Type' : 'application/x-www-form-urlencoded',
+        // 'Authorization' : 'Basic ' + btoa(process.env.REACT_APP_CLIENT_ID + ':' + process.env.REACT_APP_CLIENT_ID)     
+        'Authorization': 'Basic a8e7bf99837d4be583b6775b84b5ec87:713c72bae95a479caddcef458718abac' 
+        // Authorization: Basic *<base64 encoded client_id:client_secret>* 
+      },
+      data: 'grant_type=client_credentials',
+      method: 'POST'
+    })
+    .then(console.log)
+  // .then(response => {
+  //   setToken(response.data.access_token)
+  //   axios('https://api.spotify.com/v1/browse/categories?locale=sv_US', {
+  //       method: 'GET',
+  //       headers: { 'Authorization' : 'Bearer ' + tokenResponse.data.access_token}
+  //   })
+  //   .then(data => console.log(data))
+      
+  // })
 }, [])
   
   return(
