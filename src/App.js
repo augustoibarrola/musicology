a8cfdd06daccde0d01149cdf553b58bc907f8774
augustoibarrola <augusto.t.ibarrola@gmail.com>
2020-11-30 18:16:18 -0500

    api call to spotify works

--- src/App.js
+++ src/App.js
@@ -6,44 +6,44 @@ const App = () => {
 
   // const spotify = Credentials()
 
-  const data = [
+  const options = [
     {value: 1, name: "A" },
     {value: 2, name: "B" },
     {value: 3, name: "C" }
 ]
 
-console.log(process.env.REACT_APP_CLIENT_ID)
-
 const [token, setToken] = useState('')
+const [genre, setGenre] = useState('')
 
 useEffect(() => {
   axios('https://accounts.spotify.com/api/token', {
       headers: {
         'Content-Type' : 'application/x-www-form-urlencoded',
-        // 'Authorization' : 'Basic ' + btoa(process.env.REACT_APP_CLIENT_ID + ':' + process.env.REACT_APP_CLIENT_ID)     
-        'Authorization': 'Basic a8e7bf99837d4be583b6775b84b5ec87:713c72bae95a479caddcef458718abac' 
-        // Authorization: Basic *<base64 encoded client_id:client_secret>* 
+        'Authorization' : 'Basic ' + btoa(process.env.REACT_APP_CLIENT_ID + ':' + process.env.REACT_APP_CLIENT_SECRET)     
       },
       data: 'grant_type=client_credentials',
       method: 'POST'
     })
-    .then(console.log)
-  // .then(response => {
-  //   setToken(response.data.access_token)
-  //   axios('https://api.spotify.com/v1/browse/categories?locale=sv_US', {
-  //       method: 'GET',
-  //       headers: { 'Authorization' : 'Bearer ' + tokenResponse.data.access_token}
-  //   })
-  //   .then(data => console.log(data))
-      
-  // })
+  .then(tokenResponse => {
+    setToken(tokenResponse.data.access_token)
+
+    axios('https://api.spotify.com/v1/browse/categories?locale=sv_US',{
+      method: 'GET', 
+      headers: {
+        'Authorization' : 'Bearer ' + tokenResponse.data.access_token 
+      },
+    })
+    .then(getSearchResponse => { console.log(getSearchResponse)
+    })
+
+  })
 }, [])
   
   return(
     <div> 
       <form onSubmit={() => {}}> 
-        <Dropdown data={data}/>
-        <Dropdown data={data}/>
+        <Dropdown options={options}/>
+        <Dropdown options={options}/>
         <button type="submit" >
           search
         </button>
