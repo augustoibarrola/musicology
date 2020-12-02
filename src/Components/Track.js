b037c52025af27c9ec6865fb6e8f7052b8b934c9
augustoibarrola <augusto.t.ibarrola@gmail.com>
2020-12-02 13:03:12 -0500

    fixed up  track component

--- src/Components/Track.js
+++ src/Components/Track.js
@@ -1,10 +1,10 @@
 import React, {useState, useEffect} from 'react'
 
 const Track = ({ selectedTrack }) => {
-    console.log(selectedTrack[0].track)
 
     const [album, setAlbum] = useState({
-        name: selectedTrack[0].track.album.name, 
+        name: selectedTrack[0].track.album.name,
+        albumArtist: selectedTrack[0].track.artists[0].name, 
         image: selectedTrack[0].track.album.images[0].url,
         albumId: selectedTrack[0].track.album.id,
         releaseDate: selectedTrack[0].track.album.release_date
@@ -13,6 +13,7 @@ const Track = ({ selectedTrack }) => {
     useEffect(() => {
         setAlbum({
         name: selectedTrack[0].track.album.name, 
+        albumArtist: selectedTrack[0].track.artists[0].name, 
         image: selectedTrack[0].track.album.images[0].url,
         albumId: selectedTrack[0].track.album.id,
         releaseDate: selectedTrack[0].track.album.release_date
@@ -21,8 +22,11 @@ const Track = ({ selectedTrack }) => {
 
     return( 
         <div>
-            <img src={album.image}></img>
-            <h2>{album.name}</h2>
+            <img className="img" src={album.image}  style={{ maxHeight:'auto', maxWidth: '350px' }} ></img>
+            <div>
+                <h2>{album.name}</h2>
+                <h3> by <bold>{album.albumArtist}</bold> </h3>
+            </div>
         </div>
     )
 }
